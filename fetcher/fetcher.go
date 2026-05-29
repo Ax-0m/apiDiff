a128c58c367c484c58d37771b702a98a763598a9
Prakhar Kumar <prakharkumar1314@gmail.com>
2026-05-30 01:31:00 +0530

    add fetcher package with HTTP GET and JSON parsing

---
+++ fetcher/fetcher.go
@@ -0,0 +1,34 @@
+package fetcher
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Fetch(url string) (map[string]interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("could not fetch url %s: %w", url, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("unexpected status code %d for url %s", resp.StatusCode, url)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not read response body: %w", err)
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not parse response JSON: %w", err)
+	}
+
+	return result, nil
+}
